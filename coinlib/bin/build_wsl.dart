de8c9892131fe3084e3c9a135e877e60f4cac127
sneurlax <sneurlax@gmail.com>
2023-12-19 14:02:47 -0600

    replace processSync with execWithStdio

--- coinlib/bin/build_wsl.dart
+++ coinlib/bin/build_wsl.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'util.dart';
+
 /// Follows bitcoin-core/secp256k1's "Cross compiling" instructions.
 ///
 /// Run in WSL/WSL2.  Make sure to install the dependencies listed in the README.
@@ -7,18 +9,27 @@ import 'dart:io';
 /// apt-get install -y autoconf libtool build-essential git cmake mingw-w64
 /// ```
 
-void main() {
+void main() async {
   // Clone bitcoin-core/secp256k1.
-  Process.runSync("git", ["clone", "https://github.com/bitcoin-core/secp256k1", "src/secp256k1"]);
-  Process.runSync("git", ["checkout", "346a053d4c442e08191f075c3932d03140579d47"]);
+  await execWithStdio(
+    "git",
+    ["clone", "https://github.com/bitcoin-core/secp256k1", "src/secp256k1"],
+  );
+  await execWithStdio(
+    "git",
+    ["checkout", "346a053d4c442e08191f075c3932d03140579d47"],
+  );
 
   // Build in "src/secp256k1/lib".
   Directory("src/secp256k1/lib").createSync();
   Directory.current = Directory("src/secp256k1/lib");
 
   // Run cmake with the provided toolchain file.
-  Process.runSync("cmake", ["..", "-DCMAKE_TOOLCHAIN_FILE=../cmake/x86_64-w64-mingw32.toolchain.cmake"]);
+  await execWithStdio("cmake", [
+    "..",
+    "-DCMAKE_TOOLCHAIN_FILE=../cmake/x86_64-w64-mingw32.toolchain.cmake",
+  ]);
 
   // Build the project using "make".
-  Process.runSync("make", []);
+  await execWithStdio("make", []);
 }
