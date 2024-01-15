a813cfbc037ec97bb44ac3b1bd68d066566213ea
Matthew Mitchell <mattlmitchell@proton.me>
2024-01-15 21:58:17 +0000

    Fix cross compile Dockerfile

--- coinlib/bin/build_windows_crosscompile.dart
+++ coinlib/bin/build_windows_crosscompile.dart
@@ -8,7 +8,7 @@ FROM debian:bullseye
 
 # Install dependenices.
 RUN apt-get update -y \
-  && apt-get install -y autoconf libtool build-essential git cmake
+  && apt-get install -y autoconf libtool build-essential git cmake gcc-mingw-w64
 
 # Clone libsecp256k1 0.3.1 release.
 RUN git clone https://github.com/bitcoin-core/secp256k1 \
@@ -24,7 +24,8 @@ RUN make
 
 # Build DLL and copy into output.
 RUN make install
-RUN cp src/libsecp256k1-2.dll output/libsecp256k1.dll
+RUN mkdir output
+RUN cp src/libsecp256k1.dll output/libsecp256k1.dll
 """;
 
 void main() async {
@@ -37,7 +38,7 @@ void main() async {
     cmd,
     dockerfile,
     "coinlib_build_secp256k1_windows",
-    "/secp256k1/output/libsecp256k1.dll",
+    "output/libsecp256k1.dll",
   )) {
     exit(1);
   }
