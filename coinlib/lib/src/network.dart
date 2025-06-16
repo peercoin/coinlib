d663064e116f0f7530d97907ce3b8676752dbe30
julian <julian@cypherstack.com>
2025-06-16 10:00:16 -0600

    add toString and copyWith

--- coinlib/lib/src/network.dart
+++ coinlib/lib/src/network.dart
@@ -1,5 +1,4 @@
 class Network {
-
   final int wifPrefix, p2pkhPrefix, p2shPrefix, privHDPrefix, pubHDPrefix;
   final String bech32Hrp, messagePrefix;
   final BigInt minFee, minOutput, feePerKb;
@@ -16,6 +15,49 @@ class Network {
     required this.minOutput,
     required this.feePerKb,
   });
+
+  // copyWith function
+  Network copyWith({
+    int? wifPrefix,
+    int? p2pkhPrefix,
+    int? p2shPrefix,
+    int? privHDPrefix,
+    int? pubHDPrefix,
+    String? bech32Hrp,
+    String? messagePrefix,
+    BigInt? minFee,
+    BigInt? minOutput,
+    BigInt? feePerKb,
+  }) {
+    return Network(
+      wifPrefix: wifPrefix ?? this.wifPrefix,
+      p2pkhPrefix: p2pkhPrefix ?? this.p2pkhPrefix,
+      p2shPrefix: p2shPrefix ?? this.p2shPrefix,
+      privHDPrefix: privHDPrefix ?? this.privHDPrefix,
+      pubHDPrefix: pubHDPrefix ?? this.pubHDPrefix,
+      bech32Hrp: bech32Hrp ?? this.bech32Hrp,
+      messagePrefix: messagePrefix ?? this.messagePrefix,
+      minFee: minFee ?? this.minFee,
+      minOutput: minOutput ?? this.minOutput,
+      feePerKb: feePerKb ?? this.feePerKb,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Network(\n'
+        '  wifPrefix: $wifPrefix,\n'
+        '  p2pkhPrefix: $p2pkhPrefix,\n'
+        '  p2shPrefix: $p2shPrefix,\n'
+        '  privHDPrefix: 0x${privHDPrefix.toRadixString(16)},\n'
+        '  pubHDPrefix: 0x${pubHDPrefix.toRadixString(16)},\n'
+        '  bech32Hrp: "$bech32Hrp",\n'
+        '  messagePrefix: "$messagePrefix",\n'
+        '  minFee: $minFee,\n'
+        '  minOutput: $minOutput,\n'
+        '  feePerKb: $feePerKb\n'
+        ')';
+  }
 
   static final mainnet = Network(
     wifPrefix: 183,
@@ -42,5 +84,4 @@ class Network {
     minOutput: BigInt.from(10000),
     feePerKb: BigInt.from(10000),
   );
-
 }
