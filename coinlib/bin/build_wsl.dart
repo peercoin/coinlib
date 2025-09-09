dfbed10db9651726d9a558ceb85dbc80b39300c5
Matthew Mitchell <mattlmitchell@proton.me>
2025-09-09 14:55:23 +0100

    Guess changes required for Windows
    Requires actual testing on Windows

--- coinlib/bin/build_wsl.dart
+++ coinlib/bin/build_wsl.dart
@@ -1,47 +1,37 @@
 import 'dart:io';
-
 import 'util.dart';
 
-/// Follows bitcoin-core/secp256k1's "Cross compiling" instructions.
+/// Follows peercoin/secp256k1-coinlib's "Cross compiling" instructions.
 ///
 /// Runnable in WSL.  Install the dependencies listed in the README:
 /// ```
 /// apt-get install -y autoconf libtool build-essential git cmake mingw-w64
 /// ```
-
 void main() async {
-  // Make temporary directory.
+
   final workDir = Directory.current.path;
-  final tmpDir = createTmpDir();
 
-  // Clone bitcoin-core/secp256k1.
-  await execWithStdio(
-    "git",
-    ["clone", "https://github.com/bitcoin-core/secp256k1", "$tmpDir/secp256k1"],
-  );
-  Directory.current = Directory("$tmpDir/secp256k1");
-  await execWithStdio(
-    "git",
-    // Use version 0.5.0
-    ["checkout", "e3a885d42a7800c1ccebad94ad1e2b82c4df5c65"],
-  );
-
-  // Build in tmpDir/secp256k1/lib.
-  Directory("lib").createSync();
-  Directory.current = Directory("lib");
+  // Clone into tmp directory
+  await cloneForWindowsInTmpDir();
 
   // Run cmake with the provided toolchain file.
   await execWithStdio("cmake", [
-    "..",
+    "-B", "build",
     "-DCMAKE_TOOLCHAIN_FILE=../cmake/x86_64-w64-mingw32.toolchain.cmake",
+    "-DSECP256K1_ENABLE_MODULE_RECOVERY=ON",
+    "-DSECP256K1_BUILD_TESTS=OFF",
+    "-DSECP256K1_BUILD_EXHAUSTIVE_TESTS=OFF",
+    "-DSECP256K1_BUILD_BENCHMARK=OFF",
+    "-DSECP256K1_BUILD_EXAMPLES=OFF",
+    "-DSECP256K1_BUILD_CTIME_TESTS=OFF",
+    "-DCMAKE_BUILD_TYPE=Release",
   ]);
 
-  // Build the project using "make".
-  await execWithStdio("make", []);
+  await execWithStdio("cmake", ["--build", "build"]);
 
   // Copy the DLL to build/libsecp256k1.dll.
   Directory("$workDir/build").createSync();
-  File("src/libsecp256k1.dll").copySync("$workDir/build/secp256k1.dll");
+  File("build/bin/libsecp256k1-6.dll").copySync("$workDir/build/secp256k1.dll");
 
   print("Output libsecp256k1.dll successfully");
 
