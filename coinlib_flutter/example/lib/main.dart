cc70273ddd59ed566b42157552b7a589d16c88b1
peerchemist <dx.1178@gmail.com>
2026-09-14 14:22:05 +0200

    Remove legacy native build infrastructure

--- coinlib_flutter/example/lib/main.dart
+++ coinlib_flutter/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:coinlib_flutter/coinlib_flutter.dart' as coinlib;
 
@@ -7,9 +8,8 @@ void main() {
 }
 
 class const MyApp({super.key}) extends StatelessWidget {
-
   static String expPubkey =
-    "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
+      "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -18,17 +18,16 @@ class const MyApp({super.key}) extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10),
-          child: _getCoinLibWidget(context)
-        )
-      )
-    )
+          child: _getCoinLibWidget(context),
+        ),
+      ),
+    ),
   );
 
   Widget _getCoinLibWidget(BuildContext context) => coinlib.CoinlibLoader(
     loadChild: const Text("Loading coinlib..."),
     errorBuilder: (context, error) => Text("Error $error"),
     builder: (context) {
-
       final privKey = coinlib.ECPrivateKey.fromHex(
         "0000000000000000000000000000000000000000000000000000000000000001",
       );
@@ -46,14 +45,13 @@ class const MyApp({super.key}) extends StatelessWidget {
 
       final muSigPrivKeys = List.generate(
         2,
-        (i) => coinlib.ECPrivateKey(Uint8List(32)..last = i+1),
+        (i) => coinlib.ECPrivateKey(Uint8List(32)..last = i + 1),
       );
       final muSigPrivs = List.generate(
         2,
-        (i) => coinlib.MuSigPrivate(
-          muSigPrivKeys[i],
-          { muSigPrivKeys[otherI(i)].pubkey },
-        ),
+        (i) => coinlib.MuSigPrivate(muSigPrivKeys[i], {
+          muSigPrivKeys[otherI(i)].pubkey,
+        }),
       );
       final muSigSessions = List.generate(
         2,
@@ -67,7 +65,7 @@ class const MyApp({super.key}) extends StatelessWidget {
         (i) => muSigSessions[i].sign(
           otherNonces: {
             muSigPrivKeys[otherI(i)].pubkey:
-              muSigSessions[otherI(i)].ourPublicNonce,
+                muSigSessions[otherI(i)].ourPublicNonce,
           },
           hash: Uint8List(32),
           privKey: muSigPrivKeys[i],
@@ -79,29 +77,27 @@ class const MyApp({super.key}) extends StatelessWidget {
         partialSig: partialSigs.last,
         participantKey: muSigPrivKeys.last.pubkey,
       );
-      final adaptorSig = (
-       muSigSessions.first.finish() as coinlib.MuSigResultAdaptor
-      ).adaptorSignature;
+      final adaptorSig =
+          (muSigSessions.first.finish() as coinlib.MuSigResultAdaptor)
+              .adaptorSignature;
       final finalSig = adaptorSig.adapt(privKey);
 
       return Column(
         spacing: 10,
         children: [
           Text(
-            "Public key is ${privKey.pubkey.hex} and should equal $expPubkey."
+            "Public key is ${privKey.pubkey.hex} and should equal $expPubkey.",
           ),
           Text(
             "The message '$msg' signed with the key gives the signature"
-            " $msgSignature."
+            " $msgSignature.",
           ),
           Text(
             "An example MuSig2 Schnorr signature is"
-            " ${coinlib.bytesToHex(finalSig.data)}."
+            " ${coinlib.bytesToHex(finalSig.data)}.",
           ),
         ],
       );
-
-    }
+    },
   );
-
 }
